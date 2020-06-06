7c01630a4deb74a92fa98c5e5c6a0ba9a93be6aa
Faizul Islam <faizulfzs61@gmail.com>
2020-06-06 22:08:25 +0600

    project started.

---
+++ JPGtoPNG.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from PIL import Image
+import shutil
+
+old_folder = sys.argv[1]
+new_folder = sys.argv[2]
+
+if not os.path.exists(new_folder):
+    os.makedirs(new_folder)
+else:
+    shutil.rmtree(new_folder)           # Removes all the subdirectories!
+    os.makedirs(new_folder)
+
+for file in os.listdir(old_folder):
+    img = Image.open(f'./{old_folder}{file}')
+    clean_name = os.path.splitext(f'{file}')[0]
+    img.save(f'./{new_folder}{clean_name}.png')
+    print('conversion complete')
